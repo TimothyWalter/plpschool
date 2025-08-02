8f8080e39f2e79f6afb0b2dd80c1bd58d02e9f62
TimothyWalter <waltertimothy45@gmail.com>
2025-08-02 20:59:56 +0300

    Add files via upload

---
+++ Python/Week2/my_list.py
@@ -0,0 +1,19 @@
+#my li
+#index    (0 ,1 ,2 ,3)
+my_list = [10,20,30,40]
+list2 =[50,60,70]
+
+#insert value 15 in list
+my_list [1] = 15
+
+#extend list with [50,60,70]
+my_list.extend(list2)
+
+#remove last element
+my_list.pop(6)
+
+#sort list in ascending order
+my_list.sort()
+
+#find and print index of value 30 in list(print(my_list[]))
+print(f"index value{2}:element value{30}")
